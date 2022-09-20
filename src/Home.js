7d68fbea332f0d6e624d2cb84199a365898e4716
Ranjithkumarc0326 <ranjithkumarc2001@gmail.com>
2022-09-20 18:52:02 +0530

    first commit

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './Home.css'
+
+class Home extends React.Component{
+    render(){
+        return(
+            <div className="home">
+                <h2>Hello! I am Ranjith Kumar C</h2>
+                <p>Welcome to my portfolio.</p>
+            </div>
+        )
+    }
+}
+
+export default Home;
